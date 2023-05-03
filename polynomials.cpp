eef6243154afc411b4777d34ee5e8c11af7be962
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2023-05-03 14:56:51 -0500

    Update polynomials.cpp
    finalized methods for complex numbers, added complex root finding to the polynomial class

--- polynomials.cpp
+++ polynomials.cpp
@@ -15,17 +15,29 @@ class complex{
             return sqrt(r*r + i*i);
         };
 
-        complex pow(int i){
-
+        complex pow(int n){
+            complex result(r, i);
+            if (n==0){ 
+                result.r = 1.0;
+                result.i = 0.0;
+            }else{
+                 for(int j=1; j<n; j++) result = result*(*this);
+            };
+            return result;
         };
 
         complex conj(){
-            complex c(r, -1*i);
+            complex c(r, -1.0*i);
             return c;
         };
 
         complex operator+(complex const& c){
             complex res(r + c.r, i+c.i);
+            return res;
+        };
+
+        complex operator+(double x){
+            complex res(r + x, i);
             return res;
         };
 
@@ -46,13 +58,16 @@ class complex{
 
         complex operator/(complex c){
             complex res(r*c.r + i*c.i, i*c.r - r*c.i);
-            double n = c.norm();
+            double n = std::pow(c.norm(),2);
             res.i = res.i/n;
             res.r = res.r/n;
+            return res;
         };
 
         friend ostream& operator<<(ostream &out, const complex &c){
-            out << c.r << "+i" << c.i << endl;
+            if (c.i < 0.0) out << c.r << "-i" << abs(c.i) << endl;
+            else if (c.i == 0.0) out << c.r << endl;
+            else out << c.r << "+i" << c.i << endl;
             return out;
         };
 
@@ -67,6 +82,7 @@ class polynomial{
     public:
         polynomial(std::vector<double> coef){
             coefficients = coef;
+            deg = coef.size();
 
         };
 
@@ -74,7 +90,7 @@ class polynomial{
             double prev = guess-1.0;
             double cur = guess;
 
-            while(abs(prev-cur) > 0.000001){
+            while(abs(prev-cur) > 0.0000001){
                 prev = cur;
                 if (deriv_at(cur) == 0.0) cur += 0.00001; 
                 cur -= at(cur)/deriv_at(cur);
@@ -83,8 +99,21 @@ class polynomial{
             return cur;
         };
 
-        complex find_root(complex guess){       // find any root from starting point via newtons method
+        std::vector<complex> find_root(complex guess){    // find a real root or complex conjugate pair
+            complex prev(guess.r-1.0, guess.i-1.0);
+            complex cur = guess;
+            complex d(0.0, 0.0);
+            std::vector<complex> roots;
 
+            while(abs(prev.norm()-cur.norm()) > 0.0000001){
+                prev = cur;
+                d = c_deriv_at(cur);
+                if(d.r == 0 and d.i == 0) cur = cur + 0.01;
+                cur = cur - c_at(cur)/d;
+            };
+            roots.push_back(cur);
+            if (abs(cur.i) > 0.00001) roots.push_back(cur.conj());
+            return roots;
         };
 
         std::vector<complex> jt_roots(){        // find all roots via the Jenkins-Traub method
@@ -127,8 +156,15 @@ class polynomial{
 
 int main(){
     std::vector<double> meep = {-2.0, 0.0, 1.0};
-    polynomial boop(meep);
-    cout<<boop.at(1.0)<<endl;
-    cout<<boop.at(1.0)<<endl;
-    cout<<boop.find_real_root(1.0)<<endl;
+    polynomial beep(meep);
+    cout<<beep.at(1.0)<<endl;
+    cout<<beep.deriv_at(1.0)<<endl;
+    cout<<beep.find_real_root(1.0)<<endl;
+
+    std::vector<double> moop = {-1.0, 0.0, 0.0, 1.0};
+    polynomial boop(moop);
+    complex guess(-1.0, -1.0);
+    std::vector<complex> roots = boop.find_root(guess);
+    cout<<roots[0];
+    cout<<roots[1];
 };
