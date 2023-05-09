28c40b70c9d53ee1952c8cb52e86eda0546a0710
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2023-05-08 20:02:23 -0500

    Update polynomials.cpp
    newtons method with deflation to find all roots of a polynomial now works, still need to include polishing and a method for handling the case of repeated roots

--- polynomials.cpp
+++ polynomials.cpp
@@ -69,9 +69,9 @@ class complex{
         };
 
         friend ostream& operator<<(ostream &out, const complex &c){
-            if (c.i < 0.0) out << c.r << "-i" << abs(c.i) << endl;
-            else if (c.i == 0.0) out << c.r << endl;
-            else out << c.r << "+i" << c.i << endl;
+            if (c.i < 0.0) out << c.r << "-i" << abs(c.i);
+            else if (c.i == 0.0) out << c.r;
+            else out << c.r << "+i" << c.i;
             return out;
         };
 
@@ -122,12 +122,13 @@ class polynomial{
             vector<complex> roots;
             vector<double> temp = coefficients;
             vector<double> q;
-            double a, b, r1, r2, s1, s2;
+            double a, b, r, s;
             int num_found=0;
             int mult;
-            bool check=true;
+            bool check=false;
 
             while (num_found <= deg){
+
                 guess.r = rand()%100 -50;
                 guess.i = rand()%100 -50;
                 next = find_root(guess);
@@ -135,28 +136,27 @@ class polynomial{
 
                 if(next.i == 0){        // monomial synthetic division
                     a = next.r;
-                    r1 = coefficients[deg];
+                    r = coefficients[deg-num_found];
                     coefficients.pop_back();
-                    deg--;
-                    for(int i=deg-1; i>=0; i--){
-                        s1 = coefficients[i];
-                        coefficients[i] = r1;
-                        r1 = s1 + r1*a;
+                    for(int i=deg-1-num_found; i>=0; i--){
+                        s = coefficients[i];
+                        coefficients[i] = r;
+                        r = s + r*a;
                     };
                 }else{                  // quadratic synthetic division
                     a = next.r*next.r + next.i*next.i;
                     b = -2.0 * next.r;
                     roots.push_back(next.conj());
-                    for(int i=0; i<deg-2; i++) q.push_back(0.0);
+                    for(int i=0; i<deg-2-num_found; i++) q.push_back(0.0);
 
-                    for(int i=deg-2; i>=0; i--){
+                    for(int i=deg-2-num_found; i>=0; i--){
                         q[i] = coefficients[i+2];
                         coefficients[i+1] -= q[i]*b;
                         coefficients[i] -= q[i]*a;
                     };
-
-
-                
+                    coefficients.pop_back();
+                    coefficients.pop_back();
+                    for(int i=0; i<=deg-num_found; i++) coefficients[i] = q[i];
                 };
                 mult=1;
                 while(check){            // identify multiplicities and adjust num_found
@@ -167,6 +167,7 @@ class polynomial{
                 if (next.i==0) num_found += mult;
                 else num_found += 2*mult;
             };
+
             coefficients = temp;
             return roots;
             };
@@ -203,6 +204,11 @@ class polynomial{
 
         };
 
+        void disp(){
+            for(int i=0; i<=deg; i++) cout<<coefficients[i]<<' ';
+            cout<<endl;
+        }
+
     private:
         vector<double> coefficients;
         int deg;
@@ -222,8 +228,6 @@ int main(){
     complex root = boop.find_root(guess);
     cout<<root<<endl;
     vector<complex> roots = boop.newton_roots();
-    cout<<roots[0]<<endl;
-    cout<<roots[1]<<endl;
-    cout<<roots[2]<<endl;
+    cout<<'('<<roots[0]<<", "<<roots[1]<<", "<<roots[2]<<')'<<endl;
 
 };
