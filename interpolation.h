dcd57b1e01358d1bb4a6cc9c3429fda1fb5e44cb
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2023-06-17 20:41:17 -0500

    Update interpolation.h

--- interpolation.h
+++ interpolation.h
@@ -5,7 +5,12 @@
 #include "polynomials.h"
 using namespace std;
 
-
+// these functions will provide you with the interpolating polynomial given some set of points
 polynomial lagrange_coeff(vector<double> x, vector<double> y);
 
 polynomial newton_coeff(vector<double> x, vector<double> y);
+
+// these functions will interpolate the data to the given point "t"
+double lagrange_interp(double t, vector<double> x, vector<double> y);
+
+double newton_interp(double t, vector<double> x, vector<double> y);
