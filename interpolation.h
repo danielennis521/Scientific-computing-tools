358991063b6b4f5641792ec628c2e9073aeb93ae
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2023-09-04 18:55:44 -0500

    Update interpolation.h

--- interpolation.h
+++ interpolation.h
@@ -6,11 +6,13 @@
 using namespace std;
 
 // these functions will provide you with the interpolating polynomial given some set of points
-polynomial lagrange_coeff(vector<double> x, vector<double> y);
+polynomial lagrange_coeff(vector<double> &x, vector<double> &y);
 
-polynomial newton_coeff(vector<double> x, vector<double> y);
+polynomial newton_coeff(vector<double> &x, vector<double> &y);
+
+vector<polynomial> cubic_spline_interp(vector<double> &x, vector<double> &y);
 
 // these functions will interpolate the data to the given point "t"
-double lagrange_interp(double t, vector<double> x, vector<double> y);
+double lagrange_interp(double t, vector<double> &x, vector<double> &y);
 
-double newton_interp(double t, vector<double> x, vector<double> y);
+double newton_interp(double t, vector<double> &x, vector<double> &y);
