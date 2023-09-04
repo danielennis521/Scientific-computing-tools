4ca33cb64d3f7b3033a136887a644879d3e3ce2f
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2023-09-04 11:03:37 -0500

    Update integration.h

--- integration.h
+++ integration.h
@@ -20,14 +20,10 @@ double simpsons13_rule(double left, double right, double (*func)(double));
 double simpsons38_rule(double left, double right, double (*func)(double));
 
 
-/* fast_quadrature doesnt allow for the selection of quadrature rules but the 
+/* turbo_quadrature doesnt allow for the selection of quadrature rules but the 
 values of the function that are computed are stored and referenced in order to 
 achieve greater efficiency */
-double fast_integrate(double left, double right, double (*func)(double));
 
-double turbo_quadrature(double left, double right, double (*func)(double));
+double turbo_quadrature(double a, double b, double fa, double fb, double (*func)(double));
 
-
-/* gives the value of an integral within an order of magnitude, used for error 
-checking in adaptive quadratures */
-double integral_estimate(double left, double right, double (*func)(double));
+double integrate(double left, double right, double (*func)(double));
